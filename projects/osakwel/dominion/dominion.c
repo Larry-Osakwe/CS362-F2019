a5a6411a6205fbc497f33db52a28294cc400ae94
Larry <larryosak@gmail.com>
2019-10-10 09:33:37 -0700

    refactor mine card effect

--- projects/osakwel/dominion/dominion.c
+++ projects/osakwel/dominion/dominion.c
@@ -9,6 +9,7 @@ int baronCardEffect (struct gameState *state, int choice1, int player);
 int minionCardEffect(struct gameState *state, int handPos, int currentPlayer, int choice1, int choice2);
 int ambassadorCardEffect(int choice1, int choice2, int handPos, struct gameState *state, int currentPlayer);
 int tributeCardEffect(struct gameState *state, int nextPlayer, int currentPlayer);
+int mineCardEffect(struct gameState *state, int currentPlayer, int choice1, int choice2, int handPos);
 
 int compare(const void* a, const void* b) {
     if (*(int*)a > *(int*)b)
@@ -811,39 +812,7 @@ int cardEffect(int card, int choice1, int choice2, int choice3, struct gameState
         return -1;
 
     case mine:
-        j = state->hand[currentPlayer][choice1];  //store card we will trash
-
-        if (state->hand[currentPlayer][choice1] < copper || state->hand[currentPlayer][choice1] > gold)
-        {
-            return -1;
-        }
-
-        if (choice2 > treasure_map || choice2 < curse)
-        {
-            return -1;
-        }
-
-        if ( (getCost(state->hand[currentPlayer][choice1]) + 3) > getCost(choice2) )
-        {
-            return -1;
-        }
-
-        gainCard(choice2, state, 2, currentPlayer);
-
-        //discard card from hand
-        discardCard(handPos, currentPlayer, state, 0);
-
-        //discard trashed card
-        for (i = 0; i < state->handCount[currentPlayer]; i++)
-        {
-            if (state->hand[currentPlayer][i] == j)
-            {
-                discardCard(i, currentPlayer, state, 0);
-                break;
-            }
-        }
-
-        return 0;
+        mineCardEffect(state, currentPlayer, choice1, choice2, handPos);
 
     case remodel:
         j = state->hand[currentPlayer][choice1];  //store card we will trash
@@ -1392,6 +1361,44 @@ int tributeCardEffect(struct gameState *state, int nextPlayer, int currentPlayer
     }
 
     return 0; 
+}
+
+
+int mineCardEffect(struct gameState *state, int currentPlayer, int choice1, int choice2, int handPos)
+{
+    j = state->hand[currentPlayer][choice1];  //store card we will trash
+
+    if (state->hand[currentPlayer][choice1] < copper || state->hand[currentPlayer][choice1] > gold)
+    {
+        return -1;
+    }
+
+    if (choice2 > treasure_map || choice2 < curse)
+    {
+        return -1;
+    }
+
+    if ( (getCost(state->hand[currentPlayer][choice1]) + 3) > getCost(choice2) )
+    {
+        return -1;
+    }
+
+    gainCard(choice2, state, 2, currentPlayer);
+
+    //discard card from hand
+    discardCard(handPos, currentPlayer, state, 0);
+
+    //discard trashed card
+    for (i = 0; i < state->handCount[currentPlayer]; i++)
+    {
+        if (state->hand[currentPlayer][i] == j)
+        {
+            discardCard(i, currentPlayer, state, 0);
+            break;
+        }
+    }
+
+    return 0;    
 }
 //end of dominion.c
 
