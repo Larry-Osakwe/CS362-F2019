dfe44cc4e413b8e59b493333c16955279d6c2fb1
Larry <larryosak@gmail.com>
2019-11-10 17:07:20 -0700

    add tests to unit test for baron card

--- projects/osakwel/dominion/unittest1.c
+++ projects/osakwel/dominion/unittest1.c
@@ -17,91 +17,84 @@ void myAssert(int x, int y) {
 
 int main () {
 
-	// int k[10] = {adventurer, council_room, feast, gardens, mine, remodel, smithy, village, baron, great_hall};
-
-	// struct gameState G;
-
-	// // declarethe arrays 
-	// int coppers[MAX_HAND];
-	// int silvers[MAX_HAND];
-	// int golds[MAX_HAND];
-
-	// printf("Begin Testing baronCardEffect():\n");
-
-	// // set the state of your variables
-	// // comment on what this is going totest
-
-	// memset(&G, 23, sizeof(structgameState));   // setthe game state
-	// r = initializeGame(2, k, seed, &G); // initialize a new game
-	// G.handCount[p] = handCount;                 // setany other variables
-	// memcpy(G.hand[p], coppers, sizeof(int) * handCount); 
-
-	// // call the refactoredfunction
-	// myrefactoredfunction(p, &G, bonus);
-
-	// // verify that your assertspass, for the bugs your asserts may fail
-	// assert(G.coins == something); // check acondition
-	// assert(some condition); 
-	// assert(some condition);
-
-	// // adjustthe state of your variables to continue testing the function
-	// // comment on what this is going to test
-
-	// memset(&G, 0, sizeof(structgameState));   // setthe game state
-	// r = initializeGame(3, k, seed, &G); // initialize a new game
-	// G.handCount[p] = handCount;
-	// memcpy(G.hand[p], silvers, sizeof(int) * handCount);
-
-	// // call the refactoredfunctionagain
-	// myrefactoredfunction(p, &G, bonus);
-
-	// // verify that your assertspass, for the bugs your asserts may fail
-	// assert(G.coins == something); // check acondition
-	// assert(some condition); 
-	// assert(some condition); 
-
-	// printf("Testcompleted!\n");
-
-
-	    printf("***************Beginning Tests for Baron***************\n");
-    int seed = 10;
+	printf("Test Baron Card Effect:\n");
     int numPlayers = 2;
+    int seed = 10;
+
     //set your card array
     int k[10] = { adventurer, council_room, feast, gardens, mine, remodel, smithy, village, baron, great_hall };
 
     // declare the game states
-    struct gameState initial;
-    struct gameState G;
-    struct gameState test;
-    initializeGame(numPlayers, k, seed, &initial);
-    memcpy(&test, &initial, sizeof(struct gameState));
+    struct gameState cleanGame;
+    struct gameState compareGame;
+    struct gameState testGame;
+    initializeGame(numPlayers, k, seed, &cleanGame);
+    
+    //Test 1: Gain estate card
+    printf("\nTest: Gain estate card\n");
+    memcpy(&testGame, &cleanGame, sizeof(struct gameState));
+    testGame.hand[0][0] = baron;
+    memcpy(&compareGame, &testGame, sizeof(struct gameState));
+    baronEffect(&testGame, 0, 0, 0);
 
-    //Test1 - Discard estate card
-    printf("\n<<< Test 1 - Discard estate card >>>\n");
-    test.hand[0][0] = baron;
-    test.hand[0][1] = estate;
-    memcpy(&G, &test, sizeof(struct gameState));
-    baronEffect(1, &test, 0, 0);
     //Assert number of buys increased by 1
-    printf("Result %d Buys. Expected %d.\n", test.numBuys, G.numBuys+1);
-    assert(test.numBuys, G.numBuys+1);
-    //Assert number of gold increased by 4
-    printf("Result %d coins. Expected %d.\n", test.coins, G.coins+4);
-    myAssert(test.coins, G.coins+4);
-    //Assert number of estate cards in hand reduced by 1
-    int testEstates = 0;
-    int GEstates = 0;
-    for(int i = 0; i < test.handCount[0]; i++){
-        if (test.hand[0][i] == estate){
-            testEstates++;
-        }
-    }
-    for(int i = 0; i < G.handCount[0]; i++){
-        if (G.hand[0][i] == estate){
-            GEstates++;
-        }
-    }
-    printf("Result %d estates. Expected %d.\n", testEstates, GEstates-1);
+    printf("Results: %d Buys. Expected %d.\n", testGame.numBuys, compareGame.numBuys+1);
+    myAssert(testGame.numBuys, compareGame.numBuys+1);
 
+    //Assert gold isn't gained
+    printf("Results: %d coins. Expected %d.\n", testGame.coins, compareGame.coins);
+    myAssert(testGame.coins, compareGame.coins);
+
+    //Asset estates in discard pile increased
+    int testGameEstates = 0;
+    int compareGameEstates = 0;
+    for(int i = 0; i < testGame.handCount[0]; i++){
+        if (testGame.discard[0][i] == estate){
+            testGameEstates++;
+        }
+    }
+    for(int i = 0; i < compareGame.handCount[0]; i++){
+        if (compareGame.discard[0][i] == estate){
+            compareGameEstates++;
+        }
+    }
+    printf("Results: %d estates. Expected %d.\n", testGameEstates, compareGameEstates-1);
+    myAssert(testGameEstates, compareGameEstates+1);
+
+
+    //Test 2: Discard estate card
+    printf("\n<<< Test 1 - Discard estate card >>>\n");
+    memcpy(&testGame, &cleanGame, sizeof(struct gameState));
+    testGame.hand[0][0] = baron;
+    testGame.hand[0][1] = estate;
+    memcpy(&compareGame, &testGame, sizeof(struct gameState));
+    baronEffect(&testGame, 1, 0, 0);
+
+    //Assert number of buys increased by 1
+    printf("Results: %d Buys. Expected %d.\n", testGame.numBuys, compareGame.numBuys+1);
+    myAssert(testGame.numBuys, compareGame.numBuys+1);
+
+    //Assert number of gold increased by 4
+    printf("Results: %d coins. Expected %d.\n", testGame.coins, compareGame.coins+4);
+    myAssert(testGame.coins, compareGame.coins+4);
+
+    //Assert number of estate cards in hand reduced by 1
+    testGameEstates = 0;
+    compareGameEstates = 0;
+    for(int i = 0; i < testGame.handCount[0]; i++){
+        if (testGame.hand[0][i] == estate){
+            testGameEstates++;
+        }
+    }
+    for(int i = 0; i < compareGame.handCount[0]; i++){
+        if (compareGame.hand[0][i] == estate){
+            compareGameEstates++;
+        }
+    }
+
+    printf("Results: %d estates. Expected %d.\n", testGameEstates, compareGameEstates-1);
+    myAssert(testGameEstates, compareGameEstates-1);
+
+    return 0;
 }
 
